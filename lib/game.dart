381c050c9bf9f8168284734ff55fbd731588b3e4
XtarNodde <onyekaejimns@gmail.com>
2022-07-01 16:58:19 +0100

    navigation named

--- lib/game.dart
+++ lib/game.dart
@@ -1,57 +1,89 @@
 import 'package:flutter/material.dart';
 
 class Game extends StatefulWidget {
-  const Game({Key? key, this.name}) : super(key: key);
-final name;
+  const Game({Key? key, this.arg}) : super(key: key);
+
+  final arg;
   @override
   State<Game> createState() => _GameState();
 }
 
 class _GameState extends State<Game> {
+
   @override
   Widget build(BuildContext context) {
+    String name =widget.arg["name"];
+    String age =widget.arg["age"];
     return WillPopScope(
       onWillPop: () async {
-      final canExit= await  showDialog<bool>(
+    bool exit=   await  showDialog(
             context: context,
             builder: (context) {
-              return AlertDialog(
-                title: Text("Exit Dialog"),
-                content: SizedBox(
-                  height: 50,
-                  width: 150,
-                  child: Text(
-                    "Do you want to Exit",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
+              return  AlertDialog(
+                title: Text("Exit app"),
+                content: Text("Do you want to Exit app"),
                 actions: [
                   ElevatedButton(onPressed: () {
-Navigator.pop(context,true);
-                }, child: Text("yes")),
-                  ElevatedButton(onPressed: () {
-Navigator.pop(context,false);
-                  }, child: Text("No"))],
+                    Navigator.pop(context,true);
+                  }, child: Text("Yes")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context,false);
+                      },
+                      child: Text("No"))
+                ],
               );
             });
-
-        return canExit!;
+    print ("my exit command is $exit");
+    setState((){});
+        return exit;
       },
+
+//           () async {
+// //       final canExit= await  showDialog<bool>(
+// //             context: context,
+// //             builder: (context) {
+// //               return AlertDialog(
+// //                 title: Text("Exit Dialog"),
+// //                 content: SizedBox(
+// //                   height: 50,
+// //                   width: 150,
+// //                   child: Text(
+// //                     "Do you want to Exit",
+// //                     style: TextStyle(color: Colors.black),
+// //                   ),
+// //                 ),
+// //                 actions: [
+// //                   ElevatedButton(onPressed: () {
+// // Navigator.pop(context,true);
+// //                 }, child: Text("yes")),
+// //                   ElevatedButton(
+// //                       onPressed: () {
+// // Navigator.pop(context,false);
+// //                   },
+// //                       child: Text("No"))],
+// //               );
+// //             });
+//
+//         return false;
+//       },
+
       child: Scaffold(
-        appBar: AppBar(title: Text("${widget.name}")),
+        appBar: AppBar(title: Text("my name is $name and  i am $age yrs")),
         body: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
               onPressed: () {
-                bool can_pop = Navigator.canPop(context);
+                // bool can_pop = Navigator.canPop(context);
                 Navigator.maybePop(context);
-                if (can_pop) {
-                  Navigator.pop(context);
-                } else {
-                  print("you can't pop");
-                }
+                // if (can_pop) {
+                //   Navigator.pop(context);
+                // }
+                // else {
+                //   print("you can't pop");
+                // }
               },
               child: Text("Navigate Back"),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -5,24 +5,48 @@ import 'package:image_guesser/homePage.dart';
 import 'game.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+      MaterialApp(
     theme: ThemeData(primarySwatch: Colors.brown),
     // home: HomePage(),
-    initialRoute: "/calculator",
-    routes: {
-      "/home": (context) => HomePage(),
-      // "/calculator": (context) => calculatorMain(),
-    },
-    onGenerateRoute: (settings) {
-      final arg = settings.arguments;
-      if (settings.name == "/game") {
-        return MaterialPageRoute(
-            builder: (context) => Game(
-                  name: "${arg}",
-                ));
-      }
-      return null;
-    },
+        initialRoute: "/home",
+    // routes: {
+    //   "/home":(context)=>HomePage(),
+    //   "/game":(context)=>Game()
+    // },
+    //     routes:{} method this is same as onGenerateRoute method
+        // but  in onGenerateRoute method  you can pass a parameter with it
+        onGenerateRoute: ( routeSettings){
+    String? current_route = routeSettings.name;
+    final passed_value= routeSettings.arguments;
+    print("my curent route is = $current_route");
+    print("my pased value is = $passed_value");
+   if(current_route=="/home"){
+     return MaterialPageRoute(builder: (context) =>HomePage() ,);
+   }
+    if(current_route=="/game"){
+      return MaterialPageRoute(builder: (context) =>Game(arg:passed_value) ,);
+    }
+    else{
+    return null;}
+      },
+    // initialRoute: "/home",
+
+    // routes: {
+    //   "/home": (context) => HomePage(),
+    //   // "/calculator": (context) => calculatorMain(),
+    // },
+    // onGenerateRoute: (settings) {
+    //   final arg = settings.arguments;
+    //   if (settings.name == "/game") {
+    //     return MaterialPageRoute(
+    //         builder: (context) => Game(
+    //               name: "${arg}",
+    //             ));
+    //   }
+    //   return null;
+    // },
     debugShowCheckedModeBanner: false,
-  ));
+  )
+  );
 }
